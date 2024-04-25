17e504ba836f492e4c48c6f6881513d7e106ec89
Progress with the component <diedemiguel@gmail.com>
2024-04-25 19:21:59 +0200

    Add new files and components for Next.js routing and page rendering

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,8 @@
+export default function FilteredNewsPage({ params }) {
+  const filter = params.filter;
+  console.log(newsYear);
+  return <p>Dummy...</p>;
+  // const news = getNewsForYear(newsYear);
+
+  // return <NewsList news={news} />;
+}
