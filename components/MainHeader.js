d3948c2c613f79b32a10d11d80bb36c9d79727d2
Progress with the component <diedemiguel@gmail.com>
2024-04-25 20:13:32 +0200

    Refactor MainHeader component and update navigation links

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -1,9 +1,7 @@
-"use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
-  const path = usePathname();
   return (
     <header id="main-header">
       <div id="logo">
@@ -12,20 +10,10 @@ export default function MainHeader() {
       <nav>
         <ul>
           <li>
-            <Link
-              href="/news"
-              className={path.startsWith("/news") ? "active" : undefined}
-            >
-              News
-            </Link>
+            <NavLink href="/news">News</NavLink>
           </li>
           <li>
-            <Link
-              href="/archive"
-              className={path.startsWith("/archive") ? "active" : undefined}
-            >
-              Archive
-            </Link>
+            <NavLink href="/archive">Archive</NavLink>
           </li>
         </ul>
       </nav>
