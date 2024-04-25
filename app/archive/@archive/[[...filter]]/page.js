aaa2d4b9efaa6df4974bd3e1fa56928fa7439900
Progress with the component <diedemiguel@gmail.com>
2024-04-25 19:36:15 +0200

    Refactor archive page components and add filter functionality

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,7 +1,23 @@
+import { getAvailableNewsYears } from "@/lib/news";
+import Link from "next/link";
+
 export default function FilteredNewsPage({ params }) {
   const filter = params.filter;
-  console.log(newsYear);
-  return <p>Dummy...</p>;
+  console.log(filter);
+  const links = getAvailableNewsYears();
+  return (
+    <header id="archive-header">
+      <nav>
+        <ul>
+          {links.map((link) => (
+            <li key={link}>
+              <Link href={`/archive/${link}`}>{link}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
   // const news = getNewsForYear(newsYear);
 
   // return <NewsList news={news} />;
