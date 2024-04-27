177e5ca92c07fb6574bbd72b4be45c7a79c3dc27
Progress with the component <diedemiguel@gmail.com>
2024-04-27 15:31:53 +0200

    Refactor news page component and update data fetching

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,40 +1,16 @@
-"use client";
 import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
 
-export default function NewsPage() {
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [news, setNews] = useState([]);
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-      if (!response.ok) {
-        setError("Something went wrong!");
-        setIsLoading(false);
-        return;
-      }
-      const news = await response.json();
-      setIsLoading(false);
-      setNews(news);
-    }
-    fetchNews();
-  }, []);
-  if (isLoading) {
-    return <p>Loading...</p>;
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+  if (!response.ok) {
+    return <p>Failed to fetch news...</p>;
   }
-  if (error) {
-    return <p>{error}</p>;
-  }
-  let newsContent;
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+  const news = await response.json();
+
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 }
