bf05d7a310978bb8cf4c81e5eac8730f0536ef60
Progress with the component <diedemiguel@gmail.com>
2024-04-27 16:15:44 +0200

    Refactor InterceptedImagePage component to use async/await for data fetching

--- app/(content)/news/[slug]/@modal/(.)image/page.js
+++ app/(content)/news/[slug]/@modal/(.)image/page.js
@@ -1,13 +1,11 @@
-"use client";
-import { DUMMY_NEWS } from "@/dummy-news";
-import { notFound, useRouter } from "next/navigation";
+import ModalBackdrop from "@/components/modal-backdrop";
+import { getNewsItem } from "@/lib/news";
 
-export default function InterceptedImagePage({ params }) {
-  const router = useRouter();
+import { notFound } from "next/navigation";
+
+export default async function InterceptedImagePage({ params }) {
   const newsItemSlug = params.slug;
-  const newsItem = DUMMY_NEWS.find(
-    (newsItem) => newsItem.slug === newsItemSlug
-  );
+  const newsItem = await getNewsItem(newsItemSlug);
 
   if (!newsItem) {
     notFound();
@@ -15,7 +13,7 @@ export default function InterceptedImagePage({ params }) {
 
   return (
     <>
-      <div className="modal-backdrop" onClick={router.back} />
+      <ModalBackdrop />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img
