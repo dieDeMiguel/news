ec70545f8b870bde8d7fa1b5778ad41a3ac628c8
Progress with the component <diedemiguel@gmail.com>
2024-04-27 15:38:14 +0200

    Add better-sqlite3 dependency and update news.js to fetch data from database

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,11 +1,8 @@
 import NewsList from "@/components/news-list";
+import { getAllNews } from "@/lib/news";
 
 export default async function NewsPage() {
-  const response = await fetch("http://localhost:8080/news");
-  if (!response.ok) {
-    return <p>Failed to fetch news...</p>;
-  }
-  const news = await response.json();
+  const news = getAllNews();
 
   return (
     <>
