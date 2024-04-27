07b0c463da8414dfbb36a646bb29449bf16c51ba
Progress with the component <diedemiguel@gmail.com>
2024-04-27 16:03:58 +0200

    Refactor news page components to use async/await for data fetching

--- app/(content)/news/[slug]/page.jsx
+++ app/(content)/news/[slug]/page.jsx
@@ -1,11 +1,10 @@
+import { getNewsItem } from "@/lib/news";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-import { DUMMY_NEWS } from "@/dummy-news";
-
-export default function NewsDetailPage({ params }) {
+export default async function NewsDetailPage({ params }) {
   const newsSlug = params.slug;
-  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+  const newsItem = await getNewsItem(newsSlug);
 
   if (!newsItem) {
     notFound();
